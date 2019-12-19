5d08dc346c9e8e57667b71624d22fc5a5338131c
charlesren <renccn@gmail.com>
2019-12-19 22:35:15 +0800

    init

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"gansible/src/autologin"
+	"log"
 )
 
 func main() {
-	fmt.Println("hell")
+	fmt.Println("hello")
+	chiperList := []string{}
+	client, err := autologin.Connect("rencc", "zzb11zzb", "127.0.0.1", "", 22, chiperList)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(client)
 }
--- src/autologin/autologin.go
+++ src/autologin/autologin.go
@@ -9,7 +9,8 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-func connect(user, password, host, key string, port int, cipherList []string) (*ssh.Session, error) {
+// Connect func
+func Connect(user, password, host, key string, port int, cipherList []string) (*ssh.Session, error) {
 	var (
 		auth         []ssh.AuthMethod
 		addr         string
