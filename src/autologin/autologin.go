38d632fa2ac5caef5fb623cf49c9694e4b252793
charlesren <renccn@gmail.com>
2019-12-22 22:10:00 +0800

    init

--- src/autologin/autologin.go
+++ src/autologin/autologin.go
@@ -2,9 +2,9 @@ package autologin
 
 import (
 	"fmt"
-	"golang.org/x/crypto/ssh"
-	"net"
 	"time"
+
+	"golang.org/x/crypto/ssh"
 )
 
 // Connect func
@@ -20,13 +20,11 @@ func Connect(user string, password string, host string, port int) (*ssh.Client, 
 	auth = make([]ssh.AuthMethod, 0)
 	auth = append(auth, ssh.Password(password))
 	clientConfig = &ssh.ClientConfig{
-		User:    user,
-		Auth:    auth,
-		Timeout: 30 * time.Second,
-		Config:  config,
-		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-			return nil
-		},
+		User:            user,
+		Auth:            auth,
+		Timeout:         30 * time.Second,
+		Config:          config,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 	addr = fmt.Sprintf("%s:%d", host, port)
 	if client, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
