fa078f398587932c93a01276ca986f8c03f20c9a
rencc <renccn@gmail.com>
2019-12-27 16:26:25 +0800

    init

---
+++ cmd/shell.go
@@ -0,0 +1,94 @@
+//Package cmd ...
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"gansible/src/autologin"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
+)
+
+// shellCmd represents the shell command
+var shellCmd = &cobra.Command{
+	Use:   "shell",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("shell called")
+		passwords := []string{"abc", "passw0rd"}
+		var client *ssh.Client
+		var err error
+		for _, password := range passwords {
+			if client, err = autologin.Connect("root", password, "127.0.0.1", 22); err == nil {
+				break
+			}
+		}
+		defer client.Close()
+		session, err := client.NewSession()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer session.Close()
+		/*
+			//Exec cmd then quit
+			cmd := "date"
+			out, err := session.CombinedOutput(cmd)
+			if err != nil {
+				log.Fatal("Remote Exec Field:", err)
+			}
+			fmt.Println("Remote Exec Output:\n", string(out))
+		*/
+		//Start shell on remote host then interactive
+		session.Stdout = os.Stdout
+		session.Stderr = os.Stderr
+		session.Stdin = os.Stdin
+		modes := ssh.TerminalModes{
+			ssh.ECHO:          0,     // disable echoing
+			ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+			ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+		}
+		err = session.RequestPty("xterm-256color", 40, 80, modes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = session.Shell()
+		err = session.Wait()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(shellCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// shellCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// shellCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module gansible
 
 go 1.13
 
-require golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.6.1
+	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413
+)
