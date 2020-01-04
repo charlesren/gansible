0d9a18bcba9e09d1aa90e7e6c4cef24745eaeda2
charlesren <renccn@gmail.com>
2020-01-04 13:37:19 +0800

    init

--- cmd/run.go
+++ cmd/run.go
@@ -36,6 +36,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("run called")
 		host := args[0]
