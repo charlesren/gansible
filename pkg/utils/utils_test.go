0d5e8ea89b90a443b4fdbbdef919d1a821a704bd
rencc <renccn@gmail.com>
2020-01-06 10:17:08 +0800

    init

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,1 +1,23 @@
 package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParseIPStr(t *testing.T) {
+	var ipStr []string
+	ipStr = []string{"10.0.0.1-3", "10.0.0.1-10.0.0.3"}
+	for _, str := range ipStr {
+		ip, err := ParseIPStr(str)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if ip[0] != "10.0.0.1" {
+			t.Errorf("error")
+		}
+		if ip[2] != "10.0.0.3" {
+			t.Errorf("error")
+		}
+	}
+}
