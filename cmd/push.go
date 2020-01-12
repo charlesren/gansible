46bb4eb4b4be5842ef825401c0521cb32dfb2759
charlesren <renccn@gmail.com>
2020-01-12 22:20:39 +0800

    init

--- cmd/push.go
+++ cmd/push.go
@@ -34,13 +34,8 @@ var dest string
 // pushCmd represents the push command
 var pushCmd = &cobra.Command{
 	Use:   "push",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Upload file to remote host",
+	Long:  `Upload file to remote host.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("push called")
 		host := args[0]
