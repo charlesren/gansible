a67b35ee622b950c47c0234a4cc700b80df07daf
charlesren <renccn@gmail.com>
2020-01-12 11:38:38 +0800

    init

---
+++ cmd/script.go
@@ -0,0 +1,80 @@
+//Package cmd ...
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"gansible/pkg/utils"
+
+	"github.com/panjf2000/ants/v2"
+	"github.com/spf13/cobra"
+)
+
+var dir string
+
+// scriptCmd represents the script command
+var scriptCmd = &cobra.Command{
+	Use:   "script",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ip, err := utils.ParseIPStr(hosts)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if ip == nil {
+			fmt.Println("No hosts specified!!!")
+		} else {
+			size = 100
+			p, _ := ants.NewPool(size)
+			defer p.Release()
+			for _, host := range ip {
+				wg.Add(1)
+				_ = p.Submit(func() {
+					runr := utils.DoCommand(host, commands)
+					runinfo := utils.RunInfo(runr)
+					fmt.Println(runinfo)
+					wg.Done()
+				})
+			}
+			wg.Wait()
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(scriptCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// scriptCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// scriptCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	scriptCmd.Flags().StringVarP(&hosts, "hosts", "H", "", "eg: 10.0.0.1;10.1.1.1-3;10.2.2.2-10.2.2.5;10.3.3.1/31")
+	scriptCmd.MarkFlagRequired("hosts")
+	scriptCmd.Flags().StringVarP(&dir, "dir", "d", "", "run script at designated dir")
+}
