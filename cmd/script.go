e0ab45a8bf8862753cf92941d28810ac366b361b
charlesren <renccn@gmail.com>
2020-01-12 22:23:20 +0800

    init

--- cmd/script.go
+++ cmd/script.go
@@ -35,14 +35,9 @@ var dir string
 // scriptCmd represents the script command
 var scriptCmd = &cobra.Command{
 	Use:   "script",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.ExactArgs(1),
+	Short: "Run local script on remote hosts",
+	Long:  `Run local script on remote hosts.`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		scriptFile := args[0]
 		ip, err := utils.ParseIPStr(hosts)
