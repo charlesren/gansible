d6b1b3d6a52b933cf0e700b0905bc88394b84a67
charlesren <renccn@gmail.com>
2020-03-11 23:21:25 +0800

    add publicKey auth family

--- pkg/autologin/autologin.go
+++ pkg/autologin/autologin.go
@@ -2,9 +2,15 @@ package autologin
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
 	"time"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 )
 
 // Connect func
@@ -31,4 +37,45 @@ func Connect(user string, password string, host string, port int) (*ssh.Client, 
 		return nil, err
 	}
 	return client, nil
+}
+func publicKeyAuth(keyPath string) ssh.AuthMethod {
+	keyFile, err := homedir.Expand(keyPath)
+	if err != nil {
+		log.Fatal("find key's home dir failed", err)
+	}
+	key, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		log.Fatal("read ssh key file failed", err)
+	}
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatal("get signer failed", err)
+	}
+	return ssh.PublicKeys(signer)
+}
+
+func publicKeyWithPasswordAuth(keyPath string, keyPassword string) ssh.AuthMethod {
+	keyFile, err := homedir.Expand(keyPath)
+	if err != nil {
+		log.Fatal("find key's home dir failed", err)
+	}
+	key, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		log.Fatal("read ssh key file failed", err)
+	}
+	signer, err := ssh.ParsePrivateKeyWithPassphrase(key, []byte(keyPassword))
+	if err != nil {
+		log.Fatal("get signer failed", err)
+	}
+	return ssh.PublicKeys(signer)
+}
+
+func publicKeyWithSSHAgentAuth() ssh.AuthMethod {
+	socket := os.Getenv("SSH_AUTH_SOCK")
+	conn, err := net.Dial("unix", socket)
+	if err != nil {
+		log.Fatalf("Failed to open SSH_AUTH_SOCK: %v", err)
+	}
+	agentClient := agent.NewClient(conn)
+	return ssh.PublicKeysCallback(agentClient.Signers)
 }
